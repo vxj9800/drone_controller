6c1bc0fcee2d69f4587bab28024b47b09dc52411
vxj9800 <vatsalasitkumar.joshi@mavs.uta.edu>
2022-02-11 10:03:10 -0600

    Add definitions for the member functions in compControlTorque class
    Two member functions are defined, one is the constructor and the other is
    a callback function which is publishing the message.

--- src/compControlTorque.cpp
+++ src/compControlTorque.cpp
@@ -1,10 +1,26 @@
-#include <cstdio>
+#include "drone_controller/compControlTorque.hpp"
+
+compControlTorque::compControlTorque() : Node("compControlTorque")
+{
+	torqueVals_[0] = 0; torqueVals_[1] = 0; torqueVals_[2] = 0;
+	publisher_ = this->create_publisher<geometry_msgs::msg::Vector3>("control_torque",10);
+	timer_ = this->create_wall_timer(std::chrono::milliseconds(500),std::bind(&compControlTorque::callbackFun,this));
+}
+
+void compControlTorque::callbackFun()
+{
+	geometry_msgs::msg::Vector3 message;
+	message.x = torqueVals_[0];
+	message.y = torqueVals_[1];
+	message.z = torqueVals_[3];
+	RCLCPP_INFO(this->get_logger(),"Publishing message.");
+	publisher_->publish(message);
+}
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
-
-  printf("hello world drone_controller package\n");
-  return 0;
+	rclcpp::init(argc,argv);
+	rclcpp::spin(std::make_shared<compControlTorque>());
+	rclcpp::shutdown();
+	return 0;
 }
